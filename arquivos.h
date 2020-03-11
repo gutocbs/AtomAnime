605456adf453777cc322a02b00dd32badc3a32a9
gutocbs <gutocbsantos@yahoo.com.br>
2020-03-11 12:12:50 -0300

    0.9.5.3.1
    Not sure why it didn't push.

--- arquivos.h
+++ arquivos.h
@@ -2,40 +2,41 @@
 #define ARQUIVOS_H
 
 #include <QDebug>
-#include <QDesktopServices> //Abre episódio
-#include <QDir>
-#include <QDirIterator>     //Itera a pasta
-#include <QFile>            // Ler arquivos na pasta
-#include <QLoggingCategory> //Cria classe de log
 #include <QObject>
 #include <QPointer>
+#include <QDir>
+#include <QDirIterator>//Itera a pasta
+#include <QFile> // Ler arquivos na pasta
+#include <QDesktopServices> //Abre episódio
 #include <QUrl> //Também é usado pra abrir o episódio
+#include <QLoggingCategory> //Cria classe de log
 
 #include "anime.h"
-#include "anitomy/anitomy.h"
 #include "confusuario.h" //Retorna os diretorios de animes
-#include "logger.h"      //Mensagens de log customizadas
+#include "logger.h" //Mensagens de log customizadas
+#include "anitomy/anitomy.h"
 
-class arquivos : public QObject {
-  Q_OBJECT
+class arquivos : public QObject
+{
+    Q_OBJECT
 public:
-  explicit arquivos(QObject *parent = nullptr);
-  QString fprocuraEpisodio(anime *);
-  QString fprocuraEpisodioEspecifico(anime *, int);
-  QString fprocuraUltimoEpisodio(anime *, QString);
-  QString fremoveCaracteresDiferentes(QString);
-  bool fcomparaDadosAnime(QString, QString, QString, QStringList, int, int);
-  bool fabreEpisodio(QByteArray);
-  void frecebeDiretorios(confUsuario *);
-  void frecebeAnimes(leitorlistaanimes *);
-  int fcomparaSeasons(QString, int, int);
+    explicit arquivos(QObject *parent = nullptr);
+    QString fprocuraEpisodio(anime*);
+    QString fprocuraEpisodioEspecifico(anime*, int);
+    QString fprocuraUltimoEpisodio(anime*, QString);
+    QString fremoveCaracteresDiferentes(QString);
+    bool fcomparaDadosAnime(QString, QString, QString, QStringList, int, int);
+    bool fabreEpisodio(QByteArray);
+    void frecebeDiretorios(confUsuario*);
+    void frecebeAnimes(leitorlistaanimes*);
+    int fcomparaSeasons(QString, int, int);
 
 public slots:
 
 private:
-  QPointer<confUsuario> cconfUsuario;
-  QPointer<leitorlistaanimes> cleitorlistaanimes;
-  QVector<anime *> vlistaSelecionada;
+    QPointer<confUsuario> cconfUsuario;
+    QPointer<leitorlistaanimes> cleitorlistaanimes;
+    QVector<anime*> vlistaSelecionada;
 };
 
 #endif // ARQUIVOS_H
--- janelatorrent.h
+++ janelatorrent.h
@@ -1,69 +1,73 @@
 #ifndef JANELATORRENT_H
 #define JANELATORRENT_H
 
+#include <QWidget>
+#include <QFile>
+#include <QDir>
+#include <QDebug>
+#include <QXmlStreamReader>
+#include <QPointer>
 #include <QBoxLayout> //Pra poder por as caixas e tickar elas
 #include <QCheckBox>
-#include <QDebug>
-#include <QDesktopServices>
-#include <QDesktopServices> //Abre o site do torrent
-#include <QDir>
-#include <QFile>
-#include <QPointer>
-#include <QProcess>
-#include <QTableWidgetItem>
-#include <QTimer>
-#include <QUrl>
 #include <QWidget>
-#include <QXmlStreamReader>
+#include <QDesktopServices>
+#include <QProcess>
+#include <QTimer>
+#include <QTableWidgetItem>
+#include <QPointer>
+#include <QDesktopServices> //Abre o site do torrent
+#include <QUrl>
 
-#include "arquivos.h"
-#include "filedownloader.h"
-#include "janeladeconfig.h"
-#include "leitorlistaanimes.h"
-#include "torrentinfo.h"
 #include <anitomy/anitomy.h>
+#include "torrentinfo.h"
+#include "leitorlistaanimes.h"
+#include "janeladeconfig.h"
+#include "filedownloader.h"
+#include "arquivos.h"
+
 
 namespace Ui {
 class janelatorrent;
 }
 
-class janelatorrent : public QWidget {
-  Q_OBJECT
+class janelatorrent : public QWidget
+{
+    Q_OBJECT
 
 public:
-  explicit janelatorrent(QWidget *parent = nullptr);
-  ~janelatorrent();
+    explicit janelatorrent(QWidget *parent = nullptr);
+    ~janelatorrent();
 
-  void fpreencheTabela();
-  void fpassaPonteiros(leitorlistaanimes *, janeladeconfig *, arquivos *);
-  void fprocuraAnimeEspecifico(QString);
+    void fpreencheTabela();
+    void fpassaPonteiros(leitorlistaanimes*, janeladeconfig*, arquivos*);
+    void fprocuraAnimeEspecifico(QString);
 
 private slots:
-  void fesperaTorrent();
-  void fleXML();
-  void fbaixaTorrent();
-  void on_botaoDownload_clicked();
-  void on_botaoAtualizaLista_clicked();
+    void fesperaTorrent();
+    void fleXML();
+    void fbaixaTorrent();
+    void on_botaoDownload_clicked();
+    void on_botaoAtualizaLista_clicked();
 
-  void on_botaoInfoAnime_clicked();
+    void on_botaoInfoAnime_clicked();
 
-  void on_botaoLinkTorrent_clicked();
+    void on_botaoLinkTorrent_clicked();
 
 signals:
-  void error(QString);
-  void infoAnime(QString);
+    void error(QString);
+    void infoAnime(QString);
 
 private:
-  Ui::janelatorrent *ui;
-  anitomy::Anitomy anitomy;
-  anitomy::Anitomy anitomyCompare;
-  leitorlistaanimes *cleitor;
-  janeladeconfig *cconfig;
-  arquivos *carquivos;
-  QVector<torrentinfo *> torrent;
-  QVector<anime *> vlistaAtual;
-  QVector<int> vlistaDownload;
-  QMap<QString, QStringList> vbaixar;
+    Ui::janelatorrent *ui;
+    anitomy::Anitomy anitomy;
+    anitomy::Anitomy anitomyCompare;
+    leitorlistaanimes *cleitor;
+    janeladeconfig *cconfig;
+    arquivos *carquivos;
+    QVector<torrentinfo*> torrent;
+    QVector<anime*> vlistaAtual;
+    QVector<int> vlistaDownload;
+    QMap<QString, QStringList> vbaixar;
 };
 
 #endif // JANELATORRENT_H
